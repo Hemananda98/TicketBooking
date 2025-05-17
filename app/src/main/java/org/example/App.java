f7f1a32750b89f06fe32860396659729a549b6b0
hemananda <khreddy.cbit@gmail.com>
2025-05-17 20:16:31 +0530

    incomplete

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,10 +3,71 @@
  */
 package org.example;
 
+import org.example.entities.User;
+import org.example.services.UserBookingService;
+import org.example.util.UserServiceUtil;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.UUID;
+
 public class App {
     public static void main(String[] args) {
+        System.out.println("Running Train Booking system");
+        Scanner scanner=new Scanner(System.in);
+        int option=0;
+        UserBookingService userBookingService;
+        try{
+            userBookingService=new UserBookingService();
+
+        }catch (IOException ex){
+            System.out.println("There is something wrong");
+            return;
+        }
+        while(option!=7){
+            System.out.println("Choose option");
+            System.out.println("1. Sign up");
+            System.out.println("2. Login");
+            System.out.println("3. Fetch Bookings");
+            System.out.println("4. Search Trains");
+            System.out.println("5. Book a Seat");
+            System.out.println("6. Cancel my Booking");
+            System.out.println("7. Exit the App");
+            option = scanner.nextInt();
+            switch (option){
+                case 1:
+                    System.out.println("Enter the username to signup");
+                    String nameToSignUp=scanner.next();
+                    System.out.println("Enter the password to signup");
+                    String passwordToSignUp=scanner.next();
+                    User userToSignUp=new User(nameToSignUp,passwordToSignUp,
+                            UserServiceUtil.hashPassword(passwordToSignUp),new ArrayList<>(), UUID.randomUUID().toString());
+                    userBookingService.signUp(userToSignUp);
+                    break;
+                case 2:
+                    System.out.println("Enter the username to login");
+                    String nameToLogin=scanner.next();
+                    System.out.println("Enter the password to signup");
+                    String passwordToLogin=scanner.next();
+                    User userToLogin=new User(nameToLogin,passwordToLogin,UserServiceUtil.hashPassword(passwordToLogin),new ArrayList<>(),UUID.randomUUID().toString());
+                    try{
+                        userBookingService=new UserBookingService(userToLogin);
+
+                    }catch (IOException ex){
+                        return;
+
+                    }
+                    break;
+                case 3:
+                    System.out.println("Fetch your bookings");
+                    userBookingService.fetchBooking();
+                    break;
+                    
 
 
+            }
 
+        }
     }
 }
--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -2,12 +2,17 @@ package org.example.services;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.entities.Ticket;
 import org.example.entities.User;
+import org.example.util.UserServiceUtil;
 
 import javax.imageio.IIOException;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class UserBookingService {
 
@@ -22,7 +27,46 @@ public class UserBookingService {
 
     public UserBookingService(User user) throws IOException {
         this.user=user;
-        File users=new File(USERS_PATH);
-        userList=objectMapper.readValue(users, new TypeReference<List<User>>() {});
+        loadUsers();
     }
+
+    public UserBookingService() throws IOException{
+        loadUsers();
+    }
+
+    public List<User> loadUsers()throws IOException{
+        File users=new File(USERS_PATH);
+        return objectMapper.readValue(users, new TypeReference<List<User>>() {});
+    }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException{
+        File usersFile=new File(USERS_PATH);
+        objectMapper.writeValue(usersFile, userList);
+    }
+
+    public void fetchBooking(){
+        user.printTickets();
+    }
+
+    public Boolean cancelBooking(String ticketId ){
+        return Boolean.FALSE;
+    }
+
 }
